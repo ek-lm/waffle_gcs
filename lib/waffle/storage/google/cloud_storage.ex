8f1e0d0e4dfd17f2afd56f9a54a726e362e294f1
Eric Kim <erickim@Erics-MacBook-Pro-2.local>
2025-07-21 13:10:24 -0700

    Resolve file name.

--- lib/waffle/storage/google/cloud_storage.ex
+++ lib/waffle/storage/google/cloud_storage.ex
@@ -104,10 +104,18 @@ defmodule Waffle.Storage.Google.CloudStorage do
   Returns the full file path for the upload destination.
   """
   @spec path_for(Types.definition(), Types.version(), Types.meta()) :: String.t()
-  def path_for(definition, version, meta = {file, _scope}) do
+  def path_for(definition, version, meta) do
     definition
     |> storage_dir(version, meta)
-    |> Path.join(file.file_name)
+    |> Path.join(fullname(definition, version, meta))
+  end
+
+  @doc """
+  A wrapper for `Waffle.Definition.Versioning.resolve_file_name/3`.
+  """
+  @spec fullname(Types.definition(), Types.version(), Types.meta()) :: String.t()
+  def fullname(definition, version, meta) do
+    Waffle.Definition.Versioning.resolve_file_name(definition, version, meta)
   end
 
   @spec data({Types.file(), String.t()}) :: {:file | :binary, String.t()}
